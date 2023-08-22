91074f9b7b65d66080e8d443fe39375aef197909
Suchitta05 <suchitta.mu.66@ubu.ac.th>
2023-08-22 10:04:30 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Button, Container, Form } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
 
 function App() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, formState: { errors }, } = useForm();
   const [data, setData] = useState();
   return (
 <Container>
@@ -12,11 +12,32 @@ function App() {
   <Form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
     <Form.Group className="mb-3">
       <Form.Label>Email address</Form.Label>
-      <Form.Control {...register("email")} type="email" placeholder="Enter email" />
+      <Form.Control 
+      {...register("email", {
+        required: "required",
+        pattern: {
+          value: /\S+@\S+\.\S+/,
+          message: "รูปแบบของอีเมล์ไม่ถูกต้อง",
+        },
+      })} type="email" placeholder="Enter email" />
+    <Form.Text className="text-danger">
+      {errors.email?.message}
+    </Form.Text>
     </Form.Group>
     <Form.Group className="mb-3">
       <Form.Label>Password</Form.Label>
-      <Form.Control {...register("password")} type="password" placeholder="Enter password" />
+      <Form.Control 
+      {...register("password", {
+        required: "required",
+        minLength: {
+          value: 8,
+          message: "รหัสผ่านต้องมีความยาว 8 ตัวอักษรขึ้นไป",
+        },
+      })} 
+    type="password" placeholder="Enter password" />
+    <Form.Text className="text-danger">
+      {errors.password?.message}
+    </Form.Text>
     </Form.Group>
     <Form.Group className="mb-3">
       <Form.Label>ประเภทสมาชิก</Form.Label>
